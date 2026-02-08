5a934c04a1ca020eae6adcb8f9ab0bb4a108a33b
priscavarner@yahoo.com <priscavarner@yahoo.com>
2026-02-08 00:11:53 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,5 +9,15 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.game_mode_dropdown.items = ["CNN", "Transformer"]
 
     # Any code you write here will run before the form opens.
+
+  @handle("game_mode_dropdown_change", "change")
+  def game_mode_dropdown_change_change(self, **event_args):
+    self.game_mode = self.game_mode_dropdown.selected_value
+    if self.game_mode == "CNN":
+      anvil.server.call("run_cnn_model", board_state)
+    elif self.game_mode == "Transformer":
+      anvil.server.call("run_transformer_model", board_state)
+    pass
